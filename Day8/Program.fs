693508204ac37bf462a05342981ff55a4c9ccb76
Arnaud Van der Poorten <arnaud.vanderpoorten@sirus.be>
2020-12-10 10:09:49 +0100

    Day 8 part 3

--- Day8/Program.fs
+++ Day8/Program.fs
@@ -1,6 +1,9 @@
 ﻿type operation = Acc | Jmp | Nop
 type instruction = {operation: operation;argument:int;index:int}
 
+type result = Loop | Ends
+type executionResult = {acc:int; result:result }
+
 let parseLine (line:int*string) =
     let index = fst line
     let split= (snd line).Split(" ")
@@ -15,17 +18,39 @@ let parseLine (line:int*string) =
 
 let execute (instructions:instruction[]) =
     let rec loop (acc:int) (index:int) (visitedInstructions:Set<int>) =
-        let currentInstruction = instructions.[index]
-        printfn "%A" currentInstruction
-        if visitedInstructions.Contains(currentInstruction.index) then
-            acc
+        if index>=instructions.Length then
+            {acc=acc;result=Ends}
         else
-            let newAcc = if currentInstruction.operation = Acc then acc + currentInstruction.argument else acc
-            let newIndex = if currentInstruction.operation = Jmp then index + currentInstruction.argument else index + 1
-            let newSet = visitedInstructions.Add(index)
-            loop newAcc newIndex newSet
+            let currentInstruction = instructions.[index]
+            if visitedInstructions.Contains(currentInstruction.index) then
+                {acc=acc;result=Loop}
+            else
+                let newAcc = if currentInstruction.operation = Acc then acc + currentInstruction.argument else acc
+                let newIndex = if currentInstruction.operation = Jmp then index + currentInstruction.argument else index + 1
+                let newSet = visitedInstructions.Add(index)
+                loop newAcc newIndex newSet
 
     loop 0 0 Set.empty<int>
+
+let rec findNextIndexToChange index (instructions:instruction[]) =
+    match instructions.[index].operation with
+    | Jmp | Nop -> index
+    | _ -> findNextIndexToChange (index+1) instructions
+
+let execute2 (instructions:instruction[]) =
+    let rec loop indexToChange =
+        let newInstructions = Array.copy instructions
+        newInstructions.[indexToChange] <- match newInstructions.[indexToChange].operation with
+                                           | Jmp -> {newInstructions.[indexToChange] with operation=Nop}
+                                           | Nop -> {newInstructions.[indexToChange] with operation=Jmp}
+                                           | _ -> newInstructions.[indexToChange]
+
+        let res = execute newInstructions
+        match res.result with
+        | Ends -> res.acc
+        | Loop -> loop (findNextIndexToChange (indexToChange+1) instructions)
+
+    loop (findNextIndexToChange 0 instructions)
 
 [<EntryPoint>]
 let main argv =
@@ -41,7 +66,13 @@ let main argv =
 
     let acc = execute input
     stopwatch.Stop()
-    printfn "The accumulater just before starting a loop contains:  %i" acc
+    printfn "The accumulater just before starting a loop contains:  %i" acc.acc
+    printfn "Calculation took %ims" stopwatch.ElapsedMilliseconds
+    stopwatch.Restart()
+
+    let terminatingAcc = execute2 input
+    stopwatch.Stop()
+    printfn "The accumulater just before ending contains:  %i" terminatingAcc
     printfn "Calculation took %ims" stopwatch.ElapsedMilliseconds
     stopwatch.Restart()
     0 // return an integer exit code
